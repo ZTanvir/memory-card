f204b08c9d6a9a1b5ce4d916aa73cc65c70a6736
zahirul <zahirulopel@gmail.com>
2024-08-15 15:39:08 +0600

    featfeature: background video added to home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import mayPokemon from "./assets/video/maypokemon.mp4";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <main className="homeScreen">
+        <video
+          className="bgVideo"
+          src={mayPokemon}
+          autoPlay={true}
+          muted={true}
+          loop={true}
+        />
+      </main>
+      );
+    </>
+  );
 }
 
 export default App;
