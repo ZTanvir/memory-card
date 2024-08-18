fd8e05c13c65a8c56dd8edc9799f1a35e9c8b19d
zahirul <zahirulopel@gmail.com>
2024-08-18 15:29:55 +0600

    icon: google icon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import "./App.css";
+import Audio from "./components/Audio";
 import mayPokemon from "./assets/video/maypokemon.mp4";
+import bgMusic from "./assets/audios/bgMusic.mp3";
 
 function App() {
   return (
     <>
       <main className="homeScreen">
-        <video
+        {/* <video
           className="bgVideo"
           src={mayPokemon}
           autoPlay={true}
           muted={true}
           loop={true}
-        />
+        /> */}
       </main>
-      );
+      <footer>
+        <div className="gameBgMusic">
+          {/* <Audio src={bgMusic} controls={true} autoplay={true} /> */}
+        </div>
+        <div className="gameInstruction"></div>
+      </footer>
     </>
   );
 }
