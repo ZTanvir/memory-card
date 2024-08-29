f98aa859d2eb395e4b53c9fae08cdaf7c7114807
zahirul <zahirulopel@gmail.com>
2024-08-29 18:35:43 +0600

    feature:shuffle card every round using fisher yates algorithm

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import PokemonCard from "./PokemonCard";
+import { getRandomItem } from "../utils/helperFunctions";
 
 const GameScreen = ({ pokemonsData, initalPokemonsData, totalRound }) => {
   const [displayCard, setDisplayCard] = useState(initalPokemonsData);
@@ -8,20 +9,42 @@ const GameScreen = ({ pokemonsData, initalPokemonsData, totalRound }) => {
   const unSelectCard = (allCards, selectCard) => {
     const cardsList = allCards.filter(
       // return card that are not selected
-      (deck) => !selectCard.includes(deck.name)
+      (deck) => !selectCard.map((item) => item.name).includes(deck.name)
     );
     return cardsList;
+  };
+  // shuffle card using fisher yates shuffle algorithm
+  const shuffleCard = (cardList) => {
+    const copyItems = [...cardList];
+    for (let i = copyItems.length - 1; i >= 0; i--) {
+      const randomNum = Math.floor(Math.random() * copyItems.length);
+      if (copyItems[randomNum] !== copyItems[i]) {
+        let temp = copyItems[randomNum];
+        copyItems[randomNum] = copyItems[i];
+        copyItems[i] = temp;
+      }
+    }
+    return copyItems;
   };
 
   const handleCard = (e) => {
     const selectedCardName = e.currentTarget.dataset.cardname;
-    const allSelectedCard = [...selectedCard, selectedCardName];
-    console.log("allselectedcard:", allSelectedCard);
-    console.log(
-      "card not selected",
-      unSelectCard(pokemonsData, allSelectedCard)
+    const selectedCardData = pokemonsData.find(
+      (data) => data.name === selectedCardName
     );
+    const allSelectedCard = [...selectedCard, selectedCardData];
+    const notSelectedCard = unSelectCard(pokemonsData, allSelectedCard);
+    const newDisplayCard = [
+      getRandomItem(notSelectedCard),
+      getRandomItem(allSelectedCard),
+      getRandomItem(pokemonsData),
+    ];
+    const newDisplayCards = shuffleCard(newDisplayCard);
+
+    console.log("Shuffle card", newDisplayCard, "card", newDisplayCards);
+
     setSelectedCard([...allSelectedCard]);
+    setDisplayCard([...newDisplayCards]);
   };
   return (
     <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,6 +1,12 @@
-// generate random number between 1 to 100
+// generate random number between 1 to 500
 const generateRandomNum = () => {
   return Math.floor(Math.random() * 500 + 1);
 };
+//  generate random number between 0 to array length -1 (both included)
+const getRandomItem = (itemList) => {
+  const size = itemList.length;
+  const randomNum = Math.floor(Math.random() * size);
+  return itemList[randomNum];
+};
 
-export { generateRandomNum };
+export { generateRandomNum, getRandomItem };
